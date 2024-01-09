f6bc9fdb0274dbc400e5d19c0c3907fe22242814
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-10 01:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function nextPalindrome(num) {
   //your JS code here.
+  num++;
+  let str = num.toString();
+  let isPalindrome = false;
+
+  while (!isPalindrome) {
+    let reversedStr = str.split("").reverse().join("");
+    if (str === reversedStr) {
+      isPalindrome = true;
+    } else {
+      num++;
+      str = num.toString();
+    }
+  }
+  return num;
 }
 
-const input = prompt("Enter a palindrome number");
+function isPalindrome(num) {
+  let str = num.toString();
+  let reversedStr = str.split("").reverse().join("");
+  return str === reversedStr;
+}
+
+let input = prompt("Enter a palindrome number");
 alert(nextPalindrome(input));
